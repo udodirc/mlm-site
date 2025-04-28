d4f28d1f82ed1a22f560bc8eceb43d723f7eb433
Salmin Lev <prostoweb123@gmail.com>
2025-04-28 14:22:39 +0600

    Create BaseController. Refactor code.

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -1,20 +1,65 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Spatie\LaravelData\Data;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class BaseController extends Controller
+/**
+ * @template TService
+ * @template TModel of \Illuminate\Database\Eloquent\Model
+ * @template TResource of \Illuminate\Http\Resources\Json\JsonResource
+ * @template TCreateData of \Spatie\LaravelData\Data
+ * @template TUpdateData of \Spatie\LaravelData\Data
+ */
+abstract class BaseController extends Controller
 {
-    protected Model $model;
+    /**
+     * @var TService
+     */
+    protected mixed $service;
 
-    public function __construct(Model $model)
+    /**
+     * @var class-string<TResource>
+     */
+    protected string $resourceClass;
+
+    public function __construct(mixed $service, string $resourceClass)
     {
-        $this->model = $model;
+        $this->service = $service;
+        $this->resourceClass = $resourceClass;
     }
 
-    public function index()
+    public function index(): AnonymousResourceCollection|JsonResponse
     {
+        return ($this->resourceClass)::collection(
+            $this->service->all()
+        );
+    }
 
+    public function show(Model $model): JsonResource|JsonResponse
+    {
+        return new $this->resourceClass($model);
+    }
+
+    public function store(Data $data): JsonResource|JsonResponse
+    {
+        $model = $this->service->create($data);
+
+        return new $this->resourceClass($model);
+    }
+
+    public function update(Model $model, Data $data): JsonResource|JsonResponse
+    {
+        $model = $this->service->update($model, $data);
+
+        return new $this->resourceClass($model);
+    }
+
+    public function destroy(Model $model): bool
+    {
+        return $this->service->delete($model);
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,45 +6,14 @@ use App\Data\Admin\User\UserUpdateData;
 use App\Models\User;
 use App\Resource\UserResource;
 use App\Services\UserService;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
-class UserController extends Controller
+/**
+ * @extends BaseController<UserService, User, UserResource, UserCreateData, UserUpdateData>
+ */
+class UserController extends BaseController
 {
-    public function __construct(
-        protected UserService $userService
-    ) {}
-
-    public function index(): AnonymousResourceCollection|JsonResponse
+    public function __construct(UserService $service)
     {
-        return UserResource::collection(
-            $this->userService->all()
-        );
-    }
-
-    public function show(User $user): UserResource|JsonResponse
-    {
-        return new UserResource(
-            $user
-        );
-    }
-
-    public function store(UserCreateData $data): UserResource|JsonResponse
-    {
-        return new UserResource(
-            $this->userService->create($data)
-        );
-    }
-
-    public function update(User $user, UserUpdateData $data): UserResource|JsonResponse
-    {
-        return new UserResource(
-            $this->userService->update($user, $data)
-        );
-    }
-
-    public function destroy(User $user): bool
-    {
-        return $this->userService->delete($user);
+        parent::__construct($service, UserResource::class);
     }
 }
