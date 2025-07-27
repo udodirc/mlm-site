f7d62b5488e01bff5eaa6580aeb751b61de1951c
Salmin Lev <prostoweb123@gmail.com>
2025-07-27 12:33:13 +0600

    Fix give permissions.

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -3,16 +3,20 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\BaseController;
+use App\Resource\PermissionResource;
 use App\Services\PermissionService;
 use Spatie\Permission\Models\Permission;
 
+/**
+ * @extends BaseController<PermissionService, Permission, PermissionResource>
+ */
 class PermissionController extends BaseController
 {
     public function __construct(PermissionService $service)
     {
         parent::__construct(
             $service,
-            null,
+            PermissionResource::class,
             Permission::class,
             null,
             null,
---
+++ app/Resource/PermissionResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Resource;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Spatie\Permission\Models\Permission;
+
+/**
+ * @mixin Permission
+ */
+class PermissionResource extends JsonResource
+{
+    /**
+     * @param Request $request
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->created_at,
+        ];
+    }
+}
