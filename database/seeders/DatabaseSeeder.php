ce27db32914cbab71a7dd3d01fabe755c809fefd
Salmin Lev <prostoweb123@gmail.com>
2025-09-08 19:48:32 +0600

    Create StaticContentSeeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,7 +16,8 @@ class DatabaseSeeder extends Seeder
             PermissionSeeder::class,
             RoleSeeder::class,
             AdminPermissionSeeder::class,
-            SettingsSeeder::class
+            SettingsSeeder::class,
+            StaticContentSeeder::class
         ]);
     }
 }
