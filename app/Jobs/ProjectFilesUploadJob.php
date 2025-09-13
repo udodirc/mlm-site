d89dde9beabf489a1cfa9248754282370c6e395e
Salmin Lev <prostoweb123@gmail.com>
2025-09-13 12:20:40 +0600

    Create project upload files by job.

---
+++ app/Jobs/ProjectFilesUploadJob.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use AllowDynamicProperties;
+use App\Services\FileService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Project;
+
+#[AllowDynamicProperties] class ProjectFilesUploadJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public array $filePaths;
+    public Project $project;
+    public ?int $mainIndex;
+
+    public function __construct(array $filePaths, Project $project, ?int $mainIndex = null)
+    {
+        $this->filePaths = $filePaths;
+        $this->project = $project;
+        $this->mainIndex = $mainIndex;
+    }
+
+    public function handle(): void
+    {
+        $uploadedFiles = FileService::uploadFromTemp($this->filePaths, $this->project->id);
+        FileService::clearTempDir();
+
+        if ($this->mainIndex !== null && isset($uploadedFiles[$this->mainIndex])) {
+            if ($this->project) {
+                $this->project->main_page = $uploadedFiles[$this->mainIndex];
+                $this->project->saveQuietly();
+            }
+        }
+    }
+}
